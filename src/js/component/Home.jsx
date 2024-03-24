21775bca054d9563a2aa95e106faf465043c8d86
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-03-24 11:17:53 +0000

    First commit

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+//const Home = () => {
+	//return (
+
+
+	//);
+//};
+
+export default Home;
